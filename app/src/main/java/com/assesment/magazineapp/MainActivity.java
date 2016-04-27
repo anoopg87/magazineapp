9ddbb02d815982253ccddb3fc524e848dccc06f7
Anoop <anoopg87@gmail.com>
2016-04-28 09:57:28 +1000

    MainActivity to load NewsListFragment

--- app/src/main/java/com/assesment/magazineapp/MainActivity.java
+++ app/src/main/java/com/assesment/magazineapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.assesment.magazineapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.assesment.fragments.NewsListFragment;
+
+public class MainActivity extends AppCompatActivity  {
+
+public static String TAG="MainActivity";
+
+    /**
+     * MainActivity which loads the NewsListFragment
+     *
+     */
+
+
+    Toolbar toolbar;
+    FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        loadNewsFragment();
+    }
+
+    private void loadNewsFragment() {
+
+        mFragmentManager=getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction=mFragmentManager.beginTransaction();
+        Fragment mFragment=NewsListFragment.newInstance();
+        mFragmentTransaction.replace(R.id.fragmentLoadingspace, mFragment);
+        mFragmentTransaction.addToBackStack(mFragment.getClass().getName());
+        mFragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Fragment fragment=mFragmentManager.findFragmentById(R.id.fragmentLoadingspace);
+        if(fragment instanceof NewsListFragment){
+            finish();
+        }
     }
 }
