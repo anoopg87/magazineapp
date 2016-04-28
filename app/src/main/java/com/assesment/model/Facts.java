3c67236eb17ba2bad560303168300c7e8d5bb73b
Anoop <anoopg87@gmail.com>
2016-04-28 12:55:24 +1000

    Implement parceble

--- app/src/main/java/com/assesment/model/Facts.java
+++ app/src/main/java/com/assesment/model/Facts.java
@@ -1,95 +1,116 @@
 package com.assesment.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Facts {
+/**
+ * Model class for Facts
+ * Contain one String title and one List<Row> variables to store the values retrieved from webservice
+ * Variable name should be same as the key name of the Json response
+ *
+ * Smaple JSON Response
+ *
+ * {
+ "title":"About Canada",
+ "rows":[
+ {
+ "title":"Beavers",
+ "description":"Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony",
+ "imageHref":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
+ },
+ {
+ "title":"Flag",
+ "description":null,
+ "imageHref":"http://images.findicons.com/files/icons/662/world_flag/128/flag_of_canada.png"
+ },
+ {
+ "title":"Transportation",
+ "description":"It is a well known fact that polar bears are the main mode of transportation in Canada. They consume far less gas and have the added benefit of being difficult to steal.",
+ "imageHref":"http://1.bp.blogspot.com/_VZVOmYVm68Q/SMkzZzkGXKI/AAAAAAAAADQ/U89miaCkcyo/s400/the_golden_compass_still.jpg"
+ },
+ {
+ "title":"Hockey Night in Canada",
+ "description":"These Saturday night CBC broadcasts originally aired on radio in 1931. In 1952 they debuted on television and continue to unite (and divide) the nation each week.",
+ "imageHref":"http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg"
+ },
+ {
+ "title":"Eh",
+ "description":"A chiefly Canadian interrogative utterance, usually expressing surprise or doubt or seeking confirmation.",
+ "imageHref":null
+ },
+ {
+ "title":"Housing",
+ "description":"Warmer than you might think.",
+ "imageHref":"http://icons.iconarchive.com/icons/iconshock/alaska/256/Igloo-icon.png"
+ },
+ {
+ "title":"Public Shame",
+ "description":" Sadly it's true.",
+ "imageHref":"http://static.guim.co.uk/sys-images/Music/Pix/site_furniture/2007/04/19/avril_lavigne.jpg"
+ },
+ {
+ "title":null,
+ "description":null,
+ "imageHref":null
+ },
+ {
+ "title":"Space Program",
+ "description":"Canada hopes to soon launch a man to the moon.",
+ "imageHref":"http://files.turbosquid.com/Preview/Content_2009_07_14__10_25_15/trebucheta.jpgdf3f3bf4-935d-40ff-84b2-6ce718a327a9Larger.jpg"
+ },
+ {
+ "title":"Meese",
+ "description":"A moose is a common sight in Canada. Tall and majestic, they represent many of the values which Canadians imagine that they possess. They grow up to 2.7 metres long and can weigh over 700 kg. They swim at 10 km/h. Moose antlers weigh roughly 20 kg. The plural of moose is actually 'meese', despite what most dictionaries, encyclopedias, and experts will tell you.",
+ "imageHref":"http://caroldeckerwildlifeartstudio.net/wp-content/uploads/2011/04/IMG_2418%20majestic%20moose%201%20copy%20(Small)-96x96.jpg"
+ },
+ {
+ "title":"Geography",
+ "description":"It's really big.",
+ "imageHref":null
+ },
+ {
+ "title":"Kittens...",
+ "description":"Éare illegal. Cats are fine.",
+ "imageHref":"http://www.donegalhimalayans.com/images/That%20fish%20was%20this%20big.jpg"
+ },
+ {
+ "title":"Mounties",
+ "description":"They are the law. They are also Canada's foreign espionage service. Subtle.",
+ "imageHref":"http://3.bp.blogspot.com/__mokxbTmuJM/RnWuJ6cE9cI/AAAAAAAAATw/6z3m3w9JDiU/s400/019843_31.jpg"
+ },
+ {
+ "title":"Language",
+ "description":"Nous parlons tous les langues importants.",
+ "imageHref":null
+ }
+ ]
+ }
+ */
 
-    /**
-     * Model class for Facts
-     * Contain one String title and one List<Row> variables to store the values retrieved from webservice
-     * Variable name should be same as the key name of the Json response
-     *
-     * Smaple JSON Response
-     *
-     * {
-     "title":"About Canada",
-     "rows":[
-     {
-     "title":"Beavers",
-     "description":"Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony",
-     "imageHref":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
-     },
-     {
-     "title":"Flag",
-     "description":null,
-     "imageHref":"http://images.findicons.com/files/icons/662/world_flag/128/flag_of_canada.png"
-     },
-     {
-     "title":"Transportation",
-     "description":"It is a well known fact that polar bears are the main mode of transportation in Canada. They consume far less gas and have the added benefit of being difficult to steal.",
-     "imageHref":"http://1.bp.blogspot.com/_VZVOmYVm68Q/SMkzZzkGXKI/AAAAAAAAADQ/U89miaCkcyo/s400/the_golden_compass_still.jpg"
-     },
-     {
-     "title":"Hockey Night in Canada",
-     "description":"These Saturday night CBC broadcasts originally aired on radio in 1931. In 1952 they debuted on television and continue to unite (and divide) the nation each week.",
-     "imageHref":"http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg"
-     },
-     {
-     "title":"Eh",
-     "description":"A chiefly Canadian interrogative utterance, usually expressing surprise or doubt or seeking confirmation.",
-     "imageHref":null
-     },
-     {
-     "title":"Housing",
-     "description":"Warmer than you might think.",
-     "imageHref":"http://icons.iconarchive.com/icons/iconshock/alaska/256/Igloo-icon.png"
-     },
-     {
-     "title":"Public Shame",
-     "description":" Sadly it's true.",
-     "imageHref":"http://static.guim.co.uk/sys-images/Music/Pix/site_furniture/2007/04/19/avril_lavigne.jpg"
-     },
-     {
-     "title":null,
-     "description":null,
-     "imageHref":null
-     },
-     {
-     "title":"Space Program",
-     "description":"Canada hopes to soon launch a man to the moon.",
-     "imageHref":"http://files.turbosquid.com/Preview/Content_2009_07_14__10_25_15/trebucheta.jpgdf3f3bf4-935d-40ff-84b2-6ce718a327a9Larger.jpg"
-     },
-     {
-     "title":"Meese",
-     "description":"A moose is a common sight in Canada. Tall and majestic, they represent many of the values which Canadians imagine that they possess. They grow up to 2.7 metres long and can weigh over 700 kg. They swim at 10 km/h. Moose antlers weigh roughly 20 kg. The plural of moose is actually 'meese', despite what most dictionaries, encyclopedias, and experts will tell you.",
-     "imageHref":"http://caroldeckerwildlifeartstudio.net/wp-content/uploads/2011/04/IMG_2418%20majestic%20moose%201%20copy%20(Small)-96x96.jpg"
-     },
-     {
-     "title":"Geography",
-     "description":"It's really big.",
-     "imageHref":null
-     },
-     {
-     "title":"Kittens...",
-     "description":"Éare illegal. Cats are fine.",
-     "imageHref":"http://www.donegalhimalayans.com/images/That%20fish%20was%20this%20big.jpg"
-     },
-     {
-     "title":"Mounties",
-     "description":"They are the law. They are also Canada's foreign espionage service. Subtle.",
-     "imageHref":"http://3.bp.blogspot.com/__mokxbTmuJM/RnWuJ6cE9cI/AAAAAAAAATw/6z3m3w9JDiU/s400/019843_31.jpg"
-     },
-     {
-     "title":"Language",
-     "description":"Nous parlons tous les langues importants.",
-     "imageHref":null
-     }
-     ]
-     }
-     */
+public class Facts implements Parcelable{
+
+
 
     String title;
     List<Row> rows;
+
+    protected Facts(Parcel in) {
+        title = in.readString();
+    }
+
+    public static final Creator<Facts> CREATOR = new Creator<Facts>() {
+        @Override
+        public Facts createFromParcel(Parcel in) {
+            return new Facts(in);
+        }
+
+        @Override
+        public Facts[] newArray(int size) {
+            return new Facts[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -105,5 +126,15 @@ public class Facts {
 
     public void setRows(List<Row> rows) {
         this.rows = rows;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
     }
 }
--- app/src/main/java/com/assesment/model/Row.java
+++ app/src/main/java/com/assesment/model/Row.java
@@ -1,5 +1,8 @@
 package com.assesment.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  *  Row object of th webservice
  *  Holds three string variable one is for title another one for description and the last one for image url
@@ -13,11 +16,29 @@ package com.assesment.model;
  "imageHref":"http://images.findicons.com/files/icons/662/world_flag/128/flag_of_canada.png"
  }
  */
-public class Row {
+public class Row implements Parcelable{
 
     String title;
     String description;
     String imageHref;
+
+    protected Row(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        imageHref = in.readString();
+    }
+
+    public static final Creator<Row> CREATOR = new Creator<Row>() {
+        @Override
+        public Row createFromParcel(Parcel in) {
+            return new Row(in);
+        }
+
+        @Override
+        public Row[] newArray(int size) {
+            return new Row[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -41,5 +62,17 @@ public class Row {
 
     public void setImageHref(String imageHref) {
         this.imageHref = imageHref;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(imageHref);
     }
 }
